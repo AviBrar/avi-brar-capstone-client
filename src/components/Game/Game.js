fe11f4bbd20b4ca1d27bf56a90a946dbc4fbb8ec
Avi Brar <avineetsbrar@gmail.com>
2024-04-15 12:01:04 -0700

    game structure

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,6 +1,4 @@
-import { useState } from "react";
-import Header from "../../components/Header/Header";
-import Nav from "../../components/Nav/Nav";
+import { useState, useEffect } from "react";
 import "./Game.scss";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
@@ -15,8 +13,10 @@ export default function Game() {
   const [team2, setTeam2] = useState([]);
 
   const [leagueList, setLeagueList] = useState([]);
+  const [teamList, setTeamList] = useState([]);
 
-  const [league, setLeague] = useState("");
+  const [league, setLeague] = useState([]);
+  const [leagueId, setLeagueId] = useState([]);
 
   const [goals1, setGoals1] = useState("");
   const [goals2, setGoals2] = useState("");
@@ -51,10 +51,49 @@ export default function Game() {
     getLeagues();
   }, []);
   populateLeagues();
+  const populateTeams = () => {
+    const parent = document.getElementById("gameTeams");
+    if (!parent) {
+      return <p>Loading...</p>;
+    }
+
+    if (parent.childElementCount < teamList.length) {
+      for (let i = 0; i < teamList.length; i++) {
+        let opt = document.createElement("option");
+        opt.innerHTML = teamList[i].team_name;
+        opt.value = teamList[i].id;
+        parent.appendChild(opt);
+      }
+    }
+  };
+  useEffect(() => {
+    if (league && leagueList.length > 0) {
+      const matchedLeague = leagueList.find((l) => l.league_name === league);
+      if (matchedLeague) {
+        setLeagueId(matchedLeague.id); // Update state with the ID of the found league
+      } else {
+        setLeagueId(""); // Reset or handle the case where no league is found
+      }
+    } else {
+      setLeagueId(""); // Reset when no league is selected or if leagueList is empty
+    }
+  }, [league, leagueList]);
+  useEffect(() => {
+    const getTeams = async () => {
+      try {
+        const res = await axios.get(`${leaguesURL}/${leagueId}/teams`);
+        setTeamList(res.data);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    getTeams();
+  }, [leagueId]);
+  populateTeams();
 
   return (
     <>
-      <form className="game" onSubmit={handleSubmit}>
+      <form className="game">
         <div className="game__league">
           <div className="game__league-info">
             <label htmlFor="gameLeague">League</label>
@@ -69,9 +108,27 @@ export default function Game() {
           </div>
         </div>
         <div className="game__teams">
-          <div className="game__teams-1">
-            <label htmlFor="team1">Home Team</label>
-            <input type="" />
+          <div className="game__teams-info">
+            <label htmlFor="gameTeam1">Team 1</label>
+            <select
+              name="gameTeam1"
+              id="gameTeams"
+              className="game__teams-text dropdown"
+              onChange={(e) => setTeam1(e.target.value)}
+            >
+              <option value="invalid">Please select</option>
+            </select>
+          </div>
+          <div className="game__teams-info">
+            <label htmlFor="gameTeam2">Team 2</label>
+            <select
+              name="gameTeam2"
+              id="gameTeams"
+              className="game__teams-text dropdown"
+              onChange={(e) => setTeam2(e.target.value)}
+            >
+              <option value="invalid">Please select</option>
+            </select>
           </div>
         </div>
       </form>
